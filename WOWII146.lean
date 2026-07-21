8da05a154ae2dc686f23da5273ec333f74f6cbdd
akakabrian <akakabrian19@gmail.com>
2026-07-20 17:49:43 -1000

    test: add initial WOWII 146 proof harness

---
+++ WOWII146.lean
@@ -0,0 +1,9 @@
+import FormalConjectures.WrittenOnTheWallII.GraphConjecture146
+
+open Classical SimpleGraph
+
+#check WrittenOnTheWallII.GraphConjecture146.conjecture146
+#check graphSquare
+#check eccSet
+#check maxEccentricityVertices
+#check largestInducedTreeSize
